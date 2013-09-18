b20b4e6c4ff4bea42e7ed8ac5420131111829cf1
Ubuntu <ip-172-31-31-138.ap-northeast-1.compute.internal>
2013-09-18 14:48:04 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carrierwave::Application.config.secret_key_base = 'aa35930294da8c983d9de328b88ee89baea143d0c53dd9f54d289c2577d605aede599e0d251ea9ff2547a70c4938f006edcb426d7c439d1039a8493f82316636'
